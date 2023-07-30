774783f33479e70026fab3aaa42d51906ff5d5a8
Chirag Chakraborty <chiragofficial04@gmail.com>
2023-07-31 03:38:53 +0530

    Few changes implemented ✨

--- app/src/main/java/com/geekster/notewell/repository/UserRepository.kt
+++ app/src/main/java/com/geekster/notewell/repository/UserRepository.kt
@@ -1,38 +1,42 @@
 package com.geekster.notewell.repository
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.geekster.notewell.utlis.NetworkResult
 import com.geekster.notewell.api.UserAPI
 import com.geekster.notewell.models.UserRequest
 import com.geekster.notewell.models.UserResponse
-import com.geekster.notewell.utlis.Constants.TAG
+import org.json.JSONObject
+import retrofit2.Response
 import javax.inject.Inject
 
-class UserRepository @Inject constructor (private val userAPI : UserAPI) {
+class UserRepository @Inject constructor(private val userAPI: UserAPI) {
 
     private val _userResponseLiveData = MutableLiveData<NetworkResult<UserResponse>>()
-    val userResponseLiveData : LiveData<NetworkResult<UserResponse>>
+    val userResponseLiveData: LiveData<NetworkResult<UserResponse>>
         get() = _userResponseLiveData
 
-    suspend fun registerUser(userRequest: UserRequest){
+    suspend fun registerUser(userRequest: UserRequest) {
         _userResponseLiveData.postValue(NetworkResult.Loading())
         _userResponseLiveData.postValue(NetworkResult.Loading())
         val response = userAPI.signup(userRequest)
-        if(response.isSuccessful && response.body() != null){
+        responseHandling(response)
+    }
+
+    suspend fun loginUser(userRequest: UserRequest) {
+        val response = userAPI.signin(userRequest)
+        responseHandling(response)
+    }
+
+    private fun responseHandling(response: Response<UserResponse>) {
+        if (response.isSuccessful && response.body() != null) {
             _userResponseLiveData.postValue(NetworkResult.Success(response.body()!!))
-        }
-        else if(response.errorBody() != null){
-            _userResponseLiveData.postValue(NetworkResult.Error("Something went wrong"))
-        }
-        else{
+        } else if (response.errorBody() != null) {
+            val errorObj = JSONObject(response.errorBody()!!.charStream().readText())
+            _userResponseLiveData.postValue(NetworkResult.Error(errorObj.getString("message")))
+        } else {
             _userResponseLiveData.postValue(NetworkResult.Error("Something went wrong"))
         }
     }
 
-    suspend fun loginUser(userRequest: UserRequest){
-        val response = userAPI.signin(userRequest)
-        Log.d(TAG, response.body().toString())
-    }
 }
