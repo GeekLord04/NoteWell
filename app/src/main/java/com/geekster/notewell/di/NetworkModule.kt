f89d384e8fa8d3223f8ad8ef361b25afb10155f8
Chirag Chakraborty <chiragofficial04@gmail.com>
2023-09-15 17:22:28 +0530

    Interceptor Added ✔

--- app/src/main/java/com/geekster/notewell/di/NetworkModule.kt
+++ app/src/main/java/com/geekster/notewell/di/NetworkModule.kt
@@ -1,12 +1,16 @@
 package com.geekster.notewell.di
 
+import com.geekster.notewell.api.AuthInterceptor
+import com.geekster.notewell.api.NotesAPI
 import com.geekster.notewell.api.UserAPI
 import com.geekster.notewell.utlis.Constants.BASE_URL
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
+import retrofit2.Retrofit.Builder
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
@@ -17,16 +21,29 @@ class NetworkModule {
 
     @Singleton
     @Provides
-    fun providesRetrofit() :  Retrofit{
+    fun providesRetrofitBuilder() :  Retrofit.Builder{
         return Retrofit.Builder()
             .addConverterFactory(GsonConverterFactory.create())
             .baseUrl(BASE_URL)
-            .build()
     }
 
     @Singleton
     @Provides
-    fun providesUserAPI(retrofit: Retrofit) : UserAPI {
-        return retrofit.create(UserAPI::class.java)
+    fun provideOkHttpClient(authInterceptor: AuthInterceptor) : OkHttpClient {
+        return OkHttpClient.Builder().addInterceptor(authInterceptor).build()
+    }
+
+    @Singleton
+    @Provides
+    fun providesUserAPI(retrofitBuilder: Retrofit.Builder) : UserAPI {
+        return retrofitBuilder.build().create(UserAPI::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun providesNoteAPI(retrofitBuilder: Retrofit.Builder, okHttpClient: OkHttpClient) : NotesAPI{
+        return retrofitBuilder
+            .client(okHttpClient)
+            .build().create(NotesAPI::class.java)
     }
 }
--- app/src/main/java/com/geekster/notewell/mainFragment.kt
+++ app/src/main/java/com/geekster/notewell/mainFragment.kt
@@ -1,17 +1,34 @@
 package com.geekster.notewell
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.geekster.notewell.api.NotesAPI
+import com.geekster.notewell.utlis.Constants.TAG
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class mainFragment : Fragment() {
+
+    @Inject
+    lateinit var notesAPI: NotesAPI
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val response = notesAPI.getNotes()
+            Log.d(TAG,response.body().toString())
+        }
 
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
